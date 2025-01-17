ce10dd6a49c6ec81bc5489a131e7cdc9c595fbbe
Mircea <popescu.mircea1111@gmail.com>
2025-01-17 19:57:59 +0200

    da

---
+++ src/main/java/com/pedropathing/util/PoseMessage.java
@@ -0,0 +1,27 @@
+package com.pedropathing.util;
+
+import com.pedropathing.localization.Pose;
+
+public final class PoseMessage {
+    public long timestamp;
+    public double x;
+    public double y;
+    public double heading;
+
+    public PoseMessage(Pose pose) {
+        this.timestamp = System.nanoTime();
+        if(pose == null) {
+            this.x = 0.0;
+            this.y = 0.0;
+            this.heading = 0.0;
+        } else {
+            this.x = pose.getX();
+            this.y = pose.getY();
+            this.heading = pose.getHeading();
+        }
+    }
+
+    public String toString() {
+        return String.format("(%2.2f, %2.2f, %3.2f)", this.x, this.y, Math.toDegrees(this.heading));
+    }
+}
